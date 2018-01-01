910204f5d5c6dbc6a7fc5ef831f2a97461f3e877
jreddy <jreddy@jreddys-MacBook-Air.local>
2018-01-01 12:46:23 -0500

    init

---
+++ src/app.ios.js
@@ -0,0 +1,94 @@
+/* eslint-disable no-unused-vars */
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { Navigation } from 'react-native-navigation';
+import { Provider } from 'react-redux';
+import { registerScreens } from './screens';
+
+import { iconsMap, iconsLoaded } from './utils/AppIcons';
+import configureStore from './store/configureStore';
+
+const store = configureStore();
+
+registerScreens(store, Provider);
+
+const navigatorStyle = {
+	navBarTranslucent: true,
+	drawUnderNavBar: true,
+	navBarTextColor: 'white',
+	navBarButtonColor: 'white',
+	statusBarTextColorScheme: 'light',
+	drawUnderTabBar: true
+};
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		iconsLoaded.then(() => {
+			this.startApp();
+		});
+	}
+
+	startApp() {
+		Navigation.startTabBasedApp({
+			tabs: [
+				{
+					label: 'Movies',
+					screen: 'movieapp.Movies',
+					icon: iconsMap['ios-film-outline'],
+					selectedIcon: iconsMap['ios-film'],
+					title: 'Movies',
+					navigatorStyle,
+					navigatorButtons: {
+						rightButtons: [
+							{
+								title: 'Search',
+								id: 'search',
+								icon: iconsMap['ios-search']
+							}
+						]
+					}
+				},
+				{
+					label: 'TV Shows',
+					screen: 'movieapp.Movies',
+					icon: iconsMap['ios-desktop-outline'],
+					selectedIcon: iconsMap['ios-desktop'],
+					title: 'Movies',
+					navigatorStyle
+				},
+				{
+					label: 'Music',
+					screen: 'movieapp.Movies',
+					icon: iconsMap['ios-desktop-outline'],
+					selectedIcon: iconsMap['ios-desktop'],
+					title: 'Music',
+					navigatorStyle
+				},
+				{
+					label: 'Music2',
+					screen: 'movieapp.Movies',
+					icon: iconsMap['ios-desktop-outline'],
+					selectedIcon: iconsMap['ios-desktop'],
+					title: 'Music2',
+					navigatorStyle
+				},
+				{
+					label: 'Music3',
+					screen: 'movieapp.Movies',
+					icon: iconsMap['ios-desktop-outline'],
+					selectedIcon: iconsMap['ios-desktop'],
+					title: 'Music3',
+					navigatorStyle
+				}
+			],
+			tabsStyle: {
+				tabBarButtonColor: 'red',
+				tabBarSelectedButtonColor: 'white',
+				tabBarBackgroundColor: 'green'
+			}
+		});
+	}
+}
+
+export default App;
